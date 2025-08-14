2ce96c35e67db4756934f74d57ec17a1697ffd43
Kai Bao <kai.bao@sintef.no>
2025-08-14 14:28:27 +0200

    adding more flags to PhaseUsageInfo

--- opm/material/fluidsystems/PhaseUsageInfo.hpp
+++ opm/material/fluidsystems/PhaseUsageInfo.hpp
@@ -92,15 +92,75 @@ struct PhaseUsageInfo
                       fmt::format("Fluidsystem and PhaseUsageInfo supports 1-3 phases, but {} is active\n",
                                   numActivePhases_));
         }
+
+        const auto& phases = eclState.runspec().phases();
+
+        has_solvent = phases.active(Phase::SOLVENT);
+        has_polymer = phases.active(Phase::POLYMER);
+        has_energy = phases.active(Phase::ENERGY);
+        has_polymermw = phases.active(Phase::POLYMW);
+        has_foam = phases.active(Phase::FOAM);
+        has_brine = phases.active(Phase::BRINE);
+        has_zFraction = phases.active(Phase::ZFRACTION);
+
+        has_micp = eclState.runspec().micp();
+        has_co2_or_h2store = eclState.runspec().co2Storage() || eclState.runspec().h2Storage();
 //        initEnd();
     }
 #endif
+
+    bool hasSolvent() const noexcept {
+        return has_solvent;
+    }
+
+    bool hasPolymer() const noexcept {
+        return has_polymer;
+    }
+
+    bool hasEnergy() const noexcept {
+        return has_energy;
+    }
+
+    bool hasPolymerMW() const noexcept {
+        return has_polymermw;
+    }
+
+    bool hasFoam() const noexcept {
+        return has_foam;
+    }
+
+    bool hasBrine() const noexcept {
+        return has_brine;
+    }
+
+    bool hasZFraction() const noexcept {
+       return has_zFraction;
+    }
+
+    bool hasMICP() const noexcept {
+        return has_micp;
+    }
+
+    bool hasCO2orH2Store() const noexcept {
+        return has_co2_or_h2store;
+    }
 
 private:
     unsigned char numActivePhases_ = 0;
     std::array<bool, numPhases> phaseIsActive_ {{false, false, false}};
     std::array<short, numPhases> activeToCanonicalPhaseIdx_ {{0, 1, 2}};
     std::array<short, numPhases> canonicalToActivePhaseIdx_ {{0, 1, 2}};
+
+    bool has_solvent{};
+    bool has_polymer{};
+    bool has_energy{};
+    // polymer molecular weight
+    bool has_polymermw{};
+    bool has_foam{};
+    bool has_brine{};
+    bool has_zFraction{};
+    bool has_micp{};
+    bool has_co2_or_h2store{};
 };
 
 }
